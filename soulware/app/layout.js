198fb185444020787e78327f6548adc4a880e27e
Abhay Sharma <abhay11042004@gmail.com>
2025-10-12 19:32:16 +0530

    Fixed layout error

--- soulware/app/layout.js
+++ soulware/app/layout.js
@@ -32,61 +32,60 @@ export default function RootLayout({ children }) {
     >
       <ThemeProvider>
         <html lang="en">
-          <body
-            className={`${inter.variable} antialiased`}
-          >
+          <body className={`${inter.variable} antialiased`}>
             <Header />
-            <PageTransition>
-              {children}
-            </PageTransition>
+            <PageTransition>{children}</PageTransition>
+
             <Script
-          id="google-translate-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              function googleTranslateElementInit() {
-                new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-              }
-            `,
-          }}
-        />
-        <Script
-          src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-          strategy="afterInteractive"
-        />
-        <Script
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function() {
-                if (!window.chatbase || window.chatbase("getState") !== "initialized") {
-                  window.chatbase = (...args) => {
-                    if (!window.chatbase.q) { window.chatbase.q = []; }
-                    window.chatbase.q.push(args);
-                  };
-                  window.chatbase = new Proxy(window.chatbase, {
-                    get(target, prop) {
-                      if (prop === "q") { return target.q; }
-                      return (...args) => target(prop, ...args);
+              id="google-translate-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  function googleTranslateElementInit() {
+                    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+                  }
+                `,
+              }}
+            />
+            <Script
+              id="google-translate-loader"
+              src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+              strategy="afterInteractive"
+            />
+            <Script
+              id="chatbase-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  (function() {
+                    if (!window.chatbase || window.chatbase("getState") !== "initialized") {
+                      window.chatbase = (...args) => {
+                        if (!window.chatbase.q) { window.chatbase.q = []; }
+                        window.chatbase.q.push(args);
+                      };
+                      window.chatbase = new Proxy(window.chatbase, {
+                        get(target, prop) {
+                          if (prop === "q") { return target.q; }
+                          return (...args) => target(prop, ...args);
+                        }
+                      });
                     }
-                  });
-                }
-                const onLoad = function() {
-                  const script = document.createElement("script");
-                  script.src = "https://www.chatbase.co/embed.min.js";
-                  script.id = "MQ1SI9V9i4BPq2jnAnaKo";
-                  script.domain = "www.chatbase.co";
-                  document.body.appendChild(script);
-                };
-                if (document.readyState === "complete") {
-                  onLoad();
-                } else {
-                  window.addEventListener("load", onLoad);
-                }
-              })();
-            `,
-          }}
-        />
+                    const onLoad = function() {
+                      const script = document.createElement("script");
+                      script.src = "https://www.chatbase.co/embed.min.js";
+                      script.id = "MQ1SI9V9i4BPq2jnAnaKo";
+                      script.domain = "www.chatbase.co";
+                      document.body.appendChild(script);
+                    };
+                    if (document.readyState === "complete") {
+                      onLoad();
+                    } else {
+                      window.addEventListener("load", onLoad);
+                    }
+                  })();
+                `,
+              }}
+            />
           </body>
         </html>
       </ThemeProvider>
